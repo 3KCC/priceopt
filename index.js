bbe926cfb85a72041f6d7d80754c153d48c8c53e
Minh Trang <minhtranglamthi@gmail.com>
2015-07-20 23:01:20 +0800

    debugging

---
+++ index.js
@@ -0,0 +1,76 @@
+var request = require('request');
+var connect = require('connect');
+var http = require('http');
+var async = require('async');
+var fs = require('fs');
+var app = connect();
+
+function getYQL(pair){
+  var endpoint = 'https://query.yahooapis.com/v1/public/yql?q=';
+  var query = encodeURI('select * from yahoo.finance.xchange where pair in ("'+pair+'")');
+  var options = '&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+  return endpoint+query+options;
+}
+
+function getSingleRate(pair, callback){
+
+  var url = getYQL(pair);
+
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(null, JSON.parse(body).query.results.rate);
+    }
+    else{
+      callback(error, null);
+    }
+  });
+
+};
+
+function getAllRates(res){
+  async.series([
+    function(callback){
+      getSingleRate('USDMYR',callback);
+    },
+    function(callback){
+      getSingleRate('EURMYR',callback);
+    },
+    function(callback){
+      getSingleRate('GBPMYR',callback);
+    },
+    function(callback){
+      getSingleRate('AUDMYR',callback);
+    },
+    function(callback){
+      getSingleRate('SGDMYR',callback);
+    },
+    function(callback){
+      getSingleRate('JPYMYR',callback);
+    }
+  ], function(err, results){
+    res.end(JSON.stringify(results));
+  });
+}
+
+app.use('/rate', function(req, res){
+  //res.end(Date.now().toString());
+  getAllRates(res);
+});
+
+app.use('/', function(req, res){
+  //getAllRates(res);
+  fs.readFile('./index.html', function(error, content) {
+    if (error) {
+      res.writeHead(500);
+      res.end();
+    }
+    else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+http.createServer(app).listen(3000);
+console.log("Server start @ 3000 (HTTP)");
+
---
+++ input.js
@@ -0,0 +1,201 @@
+var _ = require('underscore');
+
+//input: number of variables(int), number of constraints(int), coefficients array (2D-array), sign array, last column array
+var Tableau = function(numOfVars, numOfCons, coef, sign, pCol, objective, objCoef){
+	this.coef = coef;
+	this.pCol = pCol;
+	this.sign = sign;
+	this.numOfVars = numOfVars;
+	this.numOfCons = numOfCons;
+	this.numOfArt = numOfArtificial(sign);
+	this.pRow = addZero(0, this.numOfCons + this.numOfArt + 1, coefSign(objective, objCoef));
+	this.pArt = addZero(this.numOfCons + this.numOfVars, 1, arrOfNum(-1, this.numOfArt));
+	this.varName = varName(numOfVars,'x');
+	this.slkName = varName(numOfCons, 'z');
+	this.artName = varName(this.numOfArt, 'y');
+	this.slack = identityMatrix(numOfCons,coefOfSlack(sign));
+	this.art = artMatrix(numOfCons, this.numOfArt, sign);
+	this.table = multiConcat([coef, this.slack, this.art, pCol]).concat([this.pRow, this.pArt]);
+	this.horizon = this.varName.concat(this.slkName, this.artName);
+	this.vertical = replaceArtificial(this.slkName, sign);
+};
+
+//input: minimize or maximize (string), coeffients array
+function coefSign(objective, coef){
+	if(objective === "minimize"){
+		coef = _.map(coef, function(value){ return value*(-1);});
+	}
+	return coef;
+}
+
+function addZero(numBefore, numAfter, arr){
+	for(var i = 0; i < numAfter; i++){
+		arr.push(0);
+	}
+
+	for(var i = 0; i < numBefore; i++){
+		arr.unshift(0);
+	}
+
+	return arr;
+}
+
+function arrOfNum(num, len){
+	var arr = [];
+	for(var i = 0; i < len; i++){
+		arr.push(num);
+	}
+	return arr;
+}
+
+//input: number of variables (int), type of them (string){x: problem variables, z: slack variables, y: artificial variables}
+//output: array of variable name
+function varName(numOfVars, type){
+	var arr = [];
+	for(var i = 1; i <= numOfVars; i++){
+		arr.push(type + i);
+	}
+	return arr;
+}
+
+function coefOfSlack(sign){
+	var arr = [];
+	for(var i in sign){
+		if(sign[i] === '='){
+			arr.push(0);
+		}else if(sign[i] === '<='){
+			arr.push(1);
+		}else {
+			arr.push(-1);
+		}
+	}
+	return arr;
+}
+
+function numOfArtificial(sign){
+	var count = 0;
+	for(var i in sign){
+		if(sign[i] === '>='){
+			count++;
+		}
+	}
+	return count;
+}
+
+function replaceArtificial(arr, sign){
+	var result = arr;
+	for(var i in sign){
+		if(sign[i] === '>='){
+			result[i] = result[i].replace('z','y');
+		}
+	}
+	return result;
+}
+
+
+//input: size of matrix (int), array of leading entries
+//out: identity matrix (2D-array)
+function identityMatrix(num,arr){
+	if(typeof(arr) !== 'undefined'){
+		if(num !== arr.length){
+			throw "Length of leading entries array is not matched."
+		}
+	}
+	var matrix = [];
+	for(var i = 0; i < num; i++){
+		matrix.push([]);
+		for(var j = 0; j < num; j++){
+			if(i===j){
+				if(typeof(arr) !== 'undefined'){
+					matrix[i].push(arr[i]);
+				}else{
+					matrix[i].push(1);
+				}
+			}else{
+				matrix[i].push(0);
+			}
+		}
+	}
+	return matrix;
+}
+
+function artMatrix(m,n,sign){
+	var result = [];
+	var pos = 0;
+	for(var i = 0; i < m; i++){
+		result.push([]);
+		for(var j = 0; j < n; j++){
+			if(sign[i] === ">="){
+				if( j === pos){
+					result[i].push(1);
+				}else{
+					result[i].push(0);
+				}
+			}else{
+				result[i].push(0);
+			}
+		}
+		pos++;
+	}
+	return result;
+}
+
+//input: 2 2D-array with the number of same rows, side = True -> put table next to each other, False -> put table one on top of others
+function concatTable(arr1, arr2){
+	var result = [];
+
+	if(arr1.length !== arr2.length){
+		throw "2 tables have different # of rows";
+	}
+	for(var i = 0; i < arr1.length; i++){
+		result.push(arr1[i].concat(arr2[i]));
+	}
+	return result;
+}
+
+function multiConcat(arr){
+	var result = arr[0];
+	for(var i = 1; i < arr.length; i++){
+		result = concatTable(result, arr[i]);
+	}
+	return result;
+}
+
+function arrangeMatrix(table, p, sign){
+	var sI = [], sII = [];
+	var tI = [], tII = [];
+	var pI = [], pII = [];
+	for(var i = 0; i < sign.length; i++){
+		if(sign[i] === ">="){ 
+			sI.push(sign[i]);
+			tI.push(table[i]);
+			pI.push(p[i]);
+		}
+		if(sign[i] !== ">="){ 
+			sII.push(sign[i]);
+			tII.push(table[i]);
+			pII.push(p[i]);
+		}
+	}
+	sign = sI.concat(sII);
+	table = tI.concat(tII);
+	p = pI.concat(pII);
+
+	return [table, p, sign];
+}
+
+/*
+var coef = [[1,1],[0,1],[2,-1]];
+var sign = ['>=','<=','>='];
+var p = [1,2,1];
+var result = arrangeMatrix(coef, p, sign);
+coef = result[0];
+p = result[1];
+sign = result[2];
+
+var prob = new Tableau(2, 3, coef, sign, p, 'minimize', [6, 3]);
+//console.log(prob);
+//console.log(arrangeMatrix(prob.coef, prob.pCol, prob.sign));
+*/
+exports.Tableau = Tableau;
+exports.arrangeMatrix = arrangeMatrix;
