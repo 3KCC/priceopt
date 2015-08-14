dda063a7b695112133bb4ab43928f1bdd871148d
Thanh Nguyen <tienthanh8490@gmail.com>
2015-08-14 14:29:43 +0800

    login

--- runServer.js
+++ runServer.js
@@ -2,13 +2,19 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var app = connect();
 var bken = require('./js/backend/main.js');
+
+var wwwhisper = require('connect-wwwhisper');
+// app holds a reference to express or connect framework, it
+// may be named differently in your source file.
+app.use(wwwhisper());
 
 app.use("/public", serveStatic(__dirname + '/public'));
 
 app.use('/settings', function(req, res){
-  //getAllRates(res);
+    //getAllRates(res);
     fs.readFile('./settings.html', function(error, content) {
       if (error) {
         res.writeHead(500);
@@ -28,6 +34,15 @@ app.use('/rate', function(req, res){
 
 app.use('/', function(req, res){
   //getAllRates(res);
+  
+    var url = require('url');
+    var url_parts = url.parse(req.url, true);
+    console.log(req.url);
+    console.log(JSON.stringify(url_parts));
+    var query = url_parts.pathname;
+
+    console.log(query);
+
     fs.readFile('./main.html', function(error, content) {
       if (error) {
         res.writeHead(500);
